a163ac8fb2f097855735965aa4da6845907dc6ae
kavya19980101 <kavyastestings@gmail.com>
2023-06-04 17:10:00 +0530

    reverse

--- src/ReverseaString3diffentway/ReverseString.java
+++ src/ReverseaString3diffentway/ReverseString.java
@@ -7,6 +7,7 @@ public class ReverseString {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a String");
         String str=sc.next();
+
         String rev="";
         int len=str.length();
         for(int i=len-1 ; i>=0 ; i--)
@@ -14,5 +15,6 @@ public class ReverseString {
             rev=rev+str.charAt(i);
         }
         System.out.println("The reverse of the string is "+rev);
+
     }
 }
