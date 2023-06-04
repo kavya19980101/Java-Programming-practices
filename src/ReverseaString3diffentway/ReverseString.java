7421f2bf64cc3dcb7e9e5045b4d9ce4be0a8b0a5
kavya19980101 <kavyastestings@gmail.com>
2023-06-04 17:36:10 +0530

    reverse

--- src/ReverseaString3diffentway/ReverseString.java
+++ src/ReverseaString3diffentway/ReverseString.java
@@ -1,20 +1,21 @@
 package ReverseaString3diffentway;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String");
-        String str=sc.next();
+        String str = sc.next();
 
-        String rev="";
-        int len=str.length();
-        for(int i=len-1 ; i>=0 ; i--)
-        {
-            rev=rev+str.charAt(i);
+        String rev = "";
+        int len = str.length();
+        for (int i = len - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
         }
-        System.out.println("The reverse of the string is "+rev);
-
+        System.out.println("The reverse of the string is " + rev);
     }
+
 }
